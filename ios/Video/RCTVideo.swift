b70b988fabbe01e32b71f0374f4fb71073170712
Michał Balawajder <michal.balawajder@miquido.com>
2024-10-03 14:57:56 +0200

    Removed Loggers.

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -1649,9 +1649,6 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
 
             if onVideoTracks != nil {
                 let videoTracks = models.composeVideoTracksSummary(for: playerItem).map { $0.asDict() }
-                if #available(iOS 14.0, *) {
-                    Logger().info("👀 \(videoTracks)")
-                }
                 self.onVideoTracks?(["videoTracks": videoTracks])
             }
         }
